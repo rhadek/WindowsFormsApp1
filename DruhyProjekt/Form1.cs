3353872b235a884c22a24f4c9cd5c0b4e55c512f
rhadek <novotnyr.05@spst.eu>
2022-02-09 09:01:29 +0100

    A_na_N-tou

---
+++ DruhyProjekt/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DruhyProjekt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int cislo1 = Convert.ToInt32(textBox1.Text);
+            int cislo2 = Convert.ToInt32(textBox2.Text);
+            int an = cislo1; ;
+            for (int i = 1;i < cislo2; i++)
+            {
+                an *= cislo1;
+            }
+            label1.Text = (an).ToString();
+        }
+    }
+}
